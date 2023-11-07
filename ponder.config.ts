7e3d826f110e2539d1fa172fc4451346e24f02b6
Salief <salieflewis@gmail.com>
2023-11-07 11:53:07 -0500

    config factory and child

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "@ponder/core";
-import { http } from "viem";
+import { http, parseAbiItem } from "viem";
 
 export const config: Config = {
   networks: [
@@ -11,10 +11,17 @@ export const config: Config = {
   ],
   contracts: [
     {
-      name: "LightAccountFactory",
+      name: "LightAccount",
       network: "optimism-goerli",
-      abi: "./abis/LightAccountFactory.json",
-      address: "0x20f2708b85292e0d90cd43c22f292b39232f1e5d",
+      abi: "./abis/LightAccount.json",
+      factory: {
+        // The address of the factory contract that creates instances of this child contract.
+        address: "0x20f2708b85292e0d90cd43c22f292b39232f1e5d",
+        // The event emitted by the factory that announces a new instance of this child contract.
+        event: parseAbiItem("event LightAccountCreated(address indexed owner, address indexed accountAddress)"),
+        // The name of the parameter that contains the address of the new child contract.
+        parameter: "accountAddress",
+      },
       startBlock: 16939705,
     },
   ],
